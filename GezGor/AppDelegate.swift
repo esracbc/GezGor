3be6753912241eccc274899bfb879df6666ed16d
esra.cebeci <esra.cebeci@trendyol.com>
2021-05-09 13:56:43 +0300

    first commit

--- GezGor/AppDelegate.swift
+++ GezGor/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import Firebase
+import GoogleMaps
+import GooglePlaces
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        GMSServices.provideAPIKey("AIzaSyAKeHsU5VYsw5AclYvjb0RvmfpcmZOIePQ")
         return true
     }
 
